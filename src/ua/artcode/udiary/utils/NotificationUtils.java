e4cdb00e805258d7055ad982fd6269018215a989
Ivpion <Ivanpiontkevich@gmail.com>
2017-10-13 20:58:25 +0300

    add mockito test for notification

--- src/ua/artcode/udiary/utils/NotificationUtils.java
+++ src/ua/artcode/udiary/utils/NotificationUtils.java
@@ -20,6 +20,7 @@ public class NotificationUtils {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(props,
